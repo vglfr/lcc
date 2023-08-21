3c696a91fc2e26e5637dc8352dc0707a1676a244
vglfr <vf.velt@gmail.com>
2023-08-21 14:13:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## LambdaIR - Lambda Calculus Compiled to Linear IR
+
+- hello world in ocaml
---
+++ shell.nix
@@ -0,0 +1,10 @@
+{
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/fb942492b7accdee4e6d17f5447091c65897dde4.tar.gz") { }
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.ocaml
+    pkgs.ocamlPackages.ocaml-lsp
+  ];
+}
